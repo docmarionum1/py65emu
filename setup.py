af4578255a3083b171b591e9ac0c1ea6bd3546a2
Jeremy Neiman <docmarionum1@gmail.com>
2022-08-05 09:27:42 -0400

    md

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
-readme = open('README.rst').read()
+readme = open('README.md').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
